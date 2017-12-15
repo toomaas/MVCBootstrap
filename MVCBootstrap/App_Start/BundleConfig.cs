a3f8635b64afab13d23fb6809de0ed835dca9254
Joaquim Tomás Almada Abreu <toomaas8@hotmail.com>
2017-12-15 11:01:38 +0000

    asp net bootstrap

--- MVCBootstrap/App_Start/BundleConfig.cs
+++ MVCBootstrap/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace MVCBootstrap
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/justified-nav.css"));
         }
     }
 }
